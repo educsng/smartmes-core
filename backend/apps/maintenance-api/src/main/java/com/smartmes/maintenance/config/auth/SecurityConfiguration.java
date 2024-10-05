72c8847db3398121909960fa853cd91d3e8feab0
Eduardo Cassânego <eduardocsng@gmail.com>
2024-10-05 16:28:19 -0300

    fix: remove unused imports

--- backend/apps/maintenance-api/src/main/java/com/smartmes/maintenance/config/auth/SecurityConfiguration.java
+++ backend/apps/maintenance-api/src/main/java/com/smartmes/maintenance/config/auth/SecurityConfiguration.java
@@ -1,21 +1,14 @@
 package com.smartmes.maintenance.config.auth;
 
 import com.smartmes.maintenance.config.auth.firebase.FirebaseTokenFilter;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
